cec74019ea33728f173f38444eff8610af3eb569
Bennett S <114384889+Bennett22518@users.noreply.github.com>
2024-09-18 11:33:58 +0530

    Add files via upload

---
+++ Music_Player/music/MusiC Ben/songs details/songlist.js
@@ -0,0 +1,39 @@
+ let song_obj=[{
+    title:"Arabic Kuthu",
+    artist:"Anirudh Ravichander Ft. Jonita Gandhi",
+    img:"../MusiC Ben/img/Beast-Tamil-2022-20220504184736-500x500.jpg",
+    audio:"../MusiC Ben/Audios/m1.mp3"
+},
+{
+    title:"Badass",
+    artist:"Song by Anirudh Ravichander, Hanuman, and Raqueeb Alam",
+    img:"../MusiC Ben/img/Badass-From-Leo-Tamil-2023-20230928162246-500x500.jpg",
+    audio:"../Audios/m2.mp3"
+},
+{
+    title:"Jalabulajangu",
+    artist:"Anirudh Ravichandran",
+    img:"../MusiC Ben/img/Don-Tamil-2022-20220512162818-500x500.jpg",
+    audio:"../Audios/m3.mp3"
+},
+{
+    title:"So Bady",
+    artist:"Song by Ananthakrrishnan and Anirudh Ravichander",
+    img:"../MusiC Ben/img/images.jpg",
+    audio:"../Audios/m4.mp3"
+},
+{
+    title:"Katchi Sera",
+    artist:"Song by Sai Abhyankkar ",
+    img:"../MusiC Ben/img/Katchi-Sera-from-Think-Indie-Tamil-2024-20240122043105-500x500.jpg",
+    audio:"../Audios/m6.mp3"
+},
+{
+    title:"Vengamavan",
+    artist:"Song by Chinnaponnu and Hiphop Tamizha",
+    img:"../MusiC Ben/img/Vengamavan.jpg.jpg",
+    audio:"../Audios/m5.mp3"
+}
+]
+export default song_obj;
+
